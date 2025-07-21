64ff853dd90a3d7929a1f30c492550d989f0d92d
Alero Awani <aleroawani18@gmail.com>
2025-07-21 17:07:30 +0100

    db

--- src/main.go
+++ src/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"Region-Simulator/config"
 	"Region-Simulator/internal/api"
-	"fmt"
 	"log"
 )
 
 func main() {
 	cfg, err := config.SetupEnv()
+	log.Printf("Config loaded: %+v\n", cfg)
 
 	if err != nil {
 		log.Fatalf("This config file is not loaded properly %v\n", err)
-		fmt.Println("The error message is:", err)
+		log.Printf("The error message is:", err)
 	}
 
 	api.StartServer(cfg)
